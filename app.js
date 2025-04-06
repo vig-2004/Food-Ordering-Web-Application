7d17d6da7d0f54b428d114c6b745183a2316e4d0
vig-2004 <pothugantivigneshvishu@gmail.com>
2025-04-06 17:51:30 +0530

    Commit Changes

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom/client";
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "Iam h1 tag!! 🚀🚀"),
-    React.createElement("h2", {}, "Iam h2 tag!!"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "Iam h1 tag!!"),
-    React.createElement("h2", {}, "Iam h2 tag!!"),
-  ]),
-]);
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello WOrld!! from React!!!"
+
+// React Element
+const Title = (
+  <h1 className="head" tabIndex="5">
+    Namste React using JSX 🚀
+  </h1>
 );
-console.log(parent);
+
+const number = 10000;
+
+const HeadingComponent = () => (
+  <div id="container">
+    {Title}
+    <h2>{10 + 1}</h2>
+    <h1 className="heading"> Namste React Functional Component</h1>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+root.render(<HeadingComponent />);
